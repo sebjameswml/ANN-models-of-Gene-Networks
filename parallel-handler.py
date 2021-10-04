15c812801204ec0ce8299730eed5c8278675a9ea
Seb James <seb.james@sheffield.ac.uk>
2021-10-04 17:15:26 +0100

    Conditionally compile

--- parallel-handler.py
+++ parallel-handler.py
@@ -42,9 +42,13 @@ print('trials: {0}'.format(trials))
 # compile the script just the once
 scriptname = "main"
 compile_cmd = "g++ --std=c++17 " + scriptname + ".cpp -o dans_genenet -I../morphologica -I/usr/include/hdf5/serial -I/usr/include/jsoncpp -Wl,-rpath,/usr/lib/x86_64-linux-gnu/hdf5/serial /usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5.so /usr/lib/x86_64-linux-gnu/libjsoncpp.a -lpthread -lsz -lz -ldl -lm -O3 -Wall"
-print ('\nCompiling program with:\n   {0}'.format(compile_cmd))
-os.system (compile_cmd)
-print ('...done.\n');
+# if main.cpp or RNet.h are newer than dans_genenet, then re-compile
+if os.path.getctime('dans_genenet') < os.path.getmtime('main.cpp') or os.path.getctime('dans_genenet') < os.path.getmtime('RNet.h'):
+    print ('\nCompiling program with:\n   {0}'.format(compile_cmd))
+    os.system (compile_cmd)
+    print ('...done.\n')
+else:
+    print ('dans_genenet binary is up to date.\n')
 
 count = 0
 for i in range(batches):
